9a419322f277e3d56bd082af4f9b71af05466807
MahmudShuvo <mhshuvo503948@gmail.com>
2024-03-26 06:05:34 +0600

    2nd

--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import {Canvas} from '@react-three/fiber'
+import {Environment,Center} from '@react-three/drei'
+import CameraRig from './CameraRig'
+import Backdrop from './Backdrop'
+import Shirt from './Shirt'
 
-const Canvas = () => {
+const CanvasModel = () => {
   return (
-    <div>Canvas</div>
+    <div>
+      <Canvas>
+        <ambientLight intensity={0.5}/>
+        <Environment preset="city"/>
+        <CameraRig>
+           {/* <Backdrop/> */}
+          <Center>
+            <Shirt/>
+          </Center>
+
+        </CameraRig> 
+      </Canvas>
+    </div>
   )
 }
 
-export default Canvas
+export default CanvasModel
